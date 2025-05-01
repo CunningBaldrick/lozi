92e182951333f9fd7da7cee2e8d14491605089f0
CunningBaldrick <baldrick@free.fr>
2025-05-01 17:16:51 +0200

    Fix compilation.
    Forgot to update this package when changing the vertex type.

--- src/transition_matrices-io.adb
+++ src/transition_matrices-io.adb
@@ -1,13 +1,15 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body Transition_Matrices.IO is
+   use Vertices;
+
    --------------
    -- Put_Line --
    --------------
    procedure Put_Line (Matrix : Transition_Matrix_Type) is
    begin
-      for Row in 1 .. Matrix.Size loop
-         for Column in 1 .. Matrix.Size loop
+      for Row in 1 .. Matrix.Last_Row loop
+         for Column in 1 .. Matrix.Last_Row loop
             if Transition_Exists (
               From   => Row,
               To     => Column,
@@ -32,7 +34,7 @@ package body Transition_Matrices.IO is
                   Put (',');
                   Need_Comma := False;
                end if;
-               Put (Positive'Image (Get_Column (This_Row, Position)));
+               Put (Vertex_Number'Image (Get_Column (This_Row, Position)));
                Position := Get_Next (This_Row, Position);
             end loop;
          end;
--- src/transition_matrices-io.ads
+++ src/transition_matrices-io.ads
@@ -1,8 +1,10 @@
+with Vertices;
+
 package Transition_Matrices.IO is
    procedure Put_Line (Matrix : Transition_Matrix_Type);
 
    procedure Put_Line (
      Matrix   : Transition_Matrix_Type;
-     Vertices : Vertex_List
+     Vertices : Standard.Vertices.Vertex_List
    );
 end Transition_Matrices.IO;
